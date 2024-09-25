fc9187eec9c11d58a381e65d9122bf294329b584
noumouaw <oumouawa.ndiaye@ynovcom>
2024-09-25 14:45:21 +0200

    modif server

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 	"ymmersion_portfolio_project/src/handlers"
 )
 
+const port = ":8088"
+
 func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
@@ -14,7 +16,8 @@ func main() {
 	http.HandleFunc("/AboutUs", handlers.HandleAboutUsPage)
 	http.HandleFunc("/Contact", handlers.HandleContactPage)
 
-	fmt.Println("Démarrage du serveur sur le port 8080")
+	fmt.Println("(http://localhost:8088) - server start on port", port)
+	http.ListenAndServe(":8088", nil)
 	err := http.ListenAndServe(":8088", nil)
 	if err != nil {
 		fmt.Println("Échec du démarrage du serveur :", err)
